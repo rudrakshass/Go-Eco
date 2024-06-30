565c953306724fa07ebeeed83874c4d335c7bb90
Samyak <samyak.coll1905@gmail.com>
2024-06-30 13:04:20 +0530

    fine tuning backend

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,7 +1,7 @@
 const {leaderboard} = require("../models/userModel");
 
 function displayLeaderboard(req,res){
-    leaderboard(req,res, ()=>{
+    leaderboard(req,res, (result)=>{
         res.render("leaderboard", {isLoggedIn : req.session.authorized, result : result});
     });
 }
