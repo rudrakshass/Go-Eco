1011042c6dc9eabba51472b3fde7f4abb9e1730d
Samyak-SH <165920966+Samyak-SH@users.noreply.github.com>
2024-06-30 13:01:33 +0530

    fine tuning backend

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,8 +1,9 @@
 const {leaderboard} = require("../models/userModel");
 
 function displayLeaderboard(req,res){
-    leaderboard(req,res);
-    // res.render("leaderboard", {isLoggedIn : req.session.authorized});
+    leaderboard(req,res, ()=>{
+        res.render("leaderboard", {isLoggedIn : req.session.authorized, result : result});
+    });
 }
 
 module.exports = {displayLeaderboard}
